7b96e36759100b9841d42017cd05b3ab03ad2e9a
Osama <Osamamehmood786@gmail.com>
2019-02-12 18:25:03 +1000

    Create a header component. Renders Nothing atm

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
+      <Header />
       <h1>Hello world</h1>
       <h2>Learn react</h2>
     </div>
