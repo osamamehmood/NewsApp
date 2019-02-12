a2019e2b1644bc19661e1f5629c0292fa323953d
Osama <Osamamehmood786@gmail.com>
2019-02-12 10:51:01 +1000

    Create a components folder and add required components to it.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>Hello world</h1>
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <h2>Learn react</h2>
+    </div>
+  );
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'))
